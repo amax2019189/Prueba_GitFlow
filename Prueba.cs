0fd9e25bf0119e8418a2f24e08a4cacb2ba3e14c
amax2019189 <amax-2019189@kinal.edu.gt>
2024-10-03 18:41:28 -0600

    Cambios Practica

---
+++ Prueba.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mensaje de Prueba");
+
+            Console.ReadLine();
+        }
+    }
+}
